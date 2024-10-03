474613b3084ec2b60bb1184d4ff5725a1cdffe6d
Humberto Ribeiro <betofoxmal@gmail.com>
2024-10-03 20:50:33 -0300

    first commit

---
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Telephone;
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ProfileUpdateRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'phone' => [
+                'required',
+                'string',
+                'max:11',
+                Rule::unique(User::class)->ignore($this->user()->id),
+            ],
+            'email' => [
+                'required',
+                'string',
+                'lowercase',
+                'email',
+                'max:255',
+                Rule::unique(User::class)->ignore($this->user()->id)
+            ],
+        ];
+    }
+}
