51e1654b43ec58e52873b959a06b9b6c3daaa8bb
Humberto Ribeiro <betofoxmal@gmail.com>
2024-10-12 05:06:34 -0300

    update width max pages

--- resources/js/Pages/Menu/Agenda.jsx
+++ resources/js/Pages/Menu/Agenda.jsx
@@ -25,7 +25,7 @@ export default function Agenda() {
         });
     };
     return (
-        <div className="w-full flex justify-start items-start">
+        <div className="max-w-[1440px] flex justify-start items-start">
             <Head title="BetoFoxNet_Info" />
             <SideBar />
             <main className="relative left-[200px] w-calc-sidebarfull max-[1080px]:w-calc-sidebarmin min-h-screen bg-gray-100 max-[1080px]:left-[70px] duration-[400ms]">
--- resources/js/Pages/Menu/ScheduleCollection.jsx
+++ resources/js/Pages/Menu/ScheduleCollection.jsx
@@ -3,7 +3,7 @@ import { Head } from "@inertiajs/react";
 
 export default function ScheduleCollection() {
     return (
-        <div className="w-full flex justify-start items-start">
+        <div className="max-w-[1440px] flex justify-start items-start">
             <Head title="BetoFoxNet_Info" />
             <SideBar />
             <main className="relative left-[200px] w-calc-sidebarfull max-[1080px]:w-calc-sidebarmin min-h-screen bg-gray-100 max-[1080px]:left-[70px] duration-[400ms]">
